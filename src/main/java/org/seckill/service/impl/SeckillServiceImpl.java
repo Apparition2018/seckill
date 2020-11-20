71a458942dae179c143b790108c3f75eea57dbf9
Apparition2018 <88850180@163.com>
2020-11-20 14:21:50 +0800

    2020-11-20 14:21:00

--- src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
+++ src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
@@ -1,6 +1,6 @@
 package org.seckill.service.impl;
 
-import org.apache.commons.collections.MapUtils;
+import org.apache.commons.collections4.MapUtils;
 import org.seckill.dao.SeckillDao;
 import org.seckill.dao.SuccessKilledDao;
 import org.seckill.dao.cache.RedisDao;
